0a890d0ae593405b3f270a661f628185feb66114
Melse <melse.code@gmail.com>
2026-08-03 12:50:07 +0800

    Initial Commit

---
+++ NeverSleep/NeverSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeverSleepApp.swift
+//  NeverSleep
+//
+//  Created by Melse on 2026/8/3.
+//
+
+import SwiftUI
+
+@main
+struct NeverSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeverSleepTests/NeverSleepTests.swift
@@ -0,0 +1,19 @@
+//
+//  NeverSleepTests.swift
+//  NeverSleepTests
+//
+//  Created by Melse on 2026/8/3.
+//
+
+import Testing
+@testable import NeverSleep
+
+struct NeverSleepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
